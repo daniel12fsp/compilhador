5d8863d2609918c939c5d2da40b9c7c8292a1ca5
Daniel <daniel@localhost.localdomain>
2015-02-14 10:27:42 -0400

    Melhorado script de execucao

--- exec.sh
+++ exec.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+rm *.class
 cd java_code/
+rm *.class
+echo $(pwd)
 java -jar ../cocor/Coco.jar -frames ../cocor/ ../gramatica_compilhador.atg 
+mv ../*.java .
 javac *.java
-java Compile ../codigo_portugol
+##java Compile ../codigo_portugol
